8f7f92c3881b647a3065092c9570c411dae85373
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-16 12:10:50 +0700

    Добавил ДЗ(Семинар 6)

---
+++ dz1/Program.cs
@@ -0,0 +1,35 @@
+﻿string GoodPrint(string input, int positiveNumberCount)
+{
+    return $"{input} >> {positiveNumberCount}";
+}
+
+string ReadUserInput(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+
+}
+
+string[] FormatInput(string input)
+{
+    return input.Replace(" ", "").Split(",");
+}
+
+int GetPositiveNumbersCount(string[] array)
+{
+    int positiveNumberCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int currentNumber = int.Parse(array[i]);
+        if (currentNumber > 0)
+        {
+            positiveNumberCount++;
+        }
+    }
+    return positiveNumberCount;
+}
+
+string input = ReadUserInput("Введите числа через запятую: ");
+string[] formattedInput = FormatInput(input);
+int positiveNumberCount = GetPositiveNumbersCount(formattedInput);
+Console.WriteLine(GoodPrint(input, positiveNumberCount));
---
+++ dz2/Program.cs
@@ -0,0 +1,28 @@
+﻿string GoodPrint(double k1, double k2, double b1, double b2, double x, double y)
+{
+    return $"k1 = {k1},b1 = {b1},k2 = {k2},b2 = {b2} >> [{x};{y}]";
+}
+
+double ReadUserInput(string text)
+{
+    Console.Write(text);
+    return double.Parse(Console.ReadLine());
+}
+
+double k1 = ReadUserInput("Введите K1: ");
+double b1 = ReadUserInput("Введите B1: ");
+double k2 = ReadUserInput("Введите K2: ");
+double b2 = ReadUserInput("Введите B2: ");
+
+
+double differenceK = k1 - k2;
+if (differenceK == 0)
+{
+    Console.WriteLine("Точка пересечения отсутствует");
+}
+else
+{
+    double x = (b2 - b1) / differenceK;
+    double y = k1 * x + b1;
+    Console.WriteLine(GoodPrint(k1,k2,b1,b2,x,y));
+}
